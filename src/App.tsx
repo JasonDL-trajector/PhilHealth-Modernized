55f8de0d4a1eb782562bbd695db9e11001afdb55
Emmanuel Jason De Lara <142410817+JasonDL-trajector@users.noreply.github.com>
2023-10-31 04:55:56 +0800

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { Grid } from '@mui/material';
+import Hero from './components/Hero/Hero';
 import Header from './components/Header/Header';
 import ImageCarousel from './components/ImageCarousel/ImageCarousel';
 import Navbar from './components/Navbar/Navbar';
+import articles from './services/articles'
 
 const appStyle = {
   backgroundImage: 'url(https://www.philhealth.gov.ph/images/bg_green.jpg)'
 }
 
 const App = () => {
+
+  const articleSection = [];
+  for (let i = 0; i > articles.length; i++) {
+    const article = articles[i];
+    articleSection.push(
+      <Hero title={article.title} body={article.body}/>
+    )
+  }
   return (
     <div className='App'
     style={appStyle}>
@@ -24,7 +35,14 @@ const App = () => {
       </div>
 
       <div>
-        Articles
+        <Grid container>
+          {
+            articles.map((article) => (
+              <Hero title={article.title} body={article.body} />
+            ))
+          }
+        </Grid>
+        
       </div>
 
       <div>
